2606aaa2fab3b84c4aee52a681d621f0c8ed2e84
Wang Kesheng <80973094+JustMuteAll@users.noreply.github.com>
2026-04-21 12:50:33 +0800

    Add analysis package exports

---
+++ src/triplen_repro/analysis/__init__.py
@@ -0,0 +1,12 @@
+from .basic import compute_basic_info
+from .fig1 import (
+    build_area_anatomy,
+    build_fig1_debug_payload,
+    build_session_illustration,
+    compare_fig1_payloads,
+    compute_population_summary,
+    run_matlab_fig1_reference,
+    write_fig1_debug_payload,
+)
+from .fig3 import compute_imagewise_analysis, compute_preference_panel, compute_psth_clusters
+from .fig5 import run_fig5, run_fig5_preflight
